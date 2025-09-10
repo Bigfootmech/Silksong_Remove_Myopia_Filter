a23fdf26957cc810e2f4f103aca8c8482eba2999
Yak <bigfootmech@gmail.com>
2025-09-10 13:36:12 +0100

    mod code, and sln

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,7 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
 
 namespace remove_myopia_filter;
 
@@ -7,11 +9,26 @@ namespace remove_myopia_filter;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
+    private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
         
     private void Awake()
     {
-        // Plugin startup logic
-        Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        harmony.PatchAll();
+    }
+    
+    [HarmonyPatch(typeof(HeroLight), "Awake")]
+    public class AtStart
+    {
+        [HarmonyPostfix]
+        static void Postfix(StartManager __instance, 
+            ref SpriteRenderer ___spriteRenderer, 
+            ref SpriteRenderer ___heroLightDonut, 
+            ref Transform ___vignette)
+        {
+            // ___spriteRenderer.enabled = false;
+            // ___heroLightDonut.enabled = false;
+            ___spriteRenderer.gameObject.active = false;
+            ___vignette.gameObject.active = false;
+        }
     }
 }
